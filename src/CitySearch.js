8655d9f575fe6017e1397898bb0085d1d756f699
Nat-crit20 <greuelnathan74@gmail.com>
2023-05-02 18:37:08 -0500

    Creted error components

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,26 +1,40 @@
 import React, { Component } from "react";
+import { InfoAlert } from "./Alert";
 
 class CitySearch extends Component {
   state = {
     query: "",
     suggestions: [],
     showSuggestions: undefined,
+    infoText: "",
   };
+
   handleInputChange = (event) => {
     const value = event.target.value;
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          "We can not find the city you are looking for. Please try another city",
+      });
+    } else {
+      this.setState({
+        query: value,
+        suggestions,
+        infoText: "",
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      suggestions: [],
       showSuggestions: false,
+      infoText: "",
     });
 
     this.props.updateEvents(suggestion);
@@ -28,6 +42,8 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
+
         <input
           type="text"
           className="city"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
+    errorText: "",
   };
   handleChange = (event) => {
     const value = event.target.value;
-
-    this.setState({
-      numberOfEvents: value,
-    });
-    this.props.updateEvents(null, value);
+    if (value <= -1) {
+      this.setState({
+        errorText: "Input must be greater than 0",
+      });
+    } else if (value > 32) {
+      this.setState({
+        errorText: "Input must be less than 32",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        errorText: "",
+      });
+      this.props.updateEvents(null, value);
+    }
   };
   render() {
     return (
@@ -21,6 +33,7 @@ class NumberOfEvents extends Component {
           className="numberInput"
           type="number"
         />
+        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
