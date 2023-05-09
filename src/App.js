a1b2e0681480e4d756e1229d7daa291b2c761ab8
Nat-crit20 <greuelnathan74@gmail.com>
2023-05-09 18:13:22 -0500

    Added setState for shoWelcomeScreen

--- src/App.js
+++ src/App.js
@@ -83,13 +83,11 @@ class App extends Component {
     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
-          this.setState({
-            events,
-            locations: extractLocations(events),
-          });
+          this.setState({ events, locations: extractLocations(events) });
         }
       });
     }
